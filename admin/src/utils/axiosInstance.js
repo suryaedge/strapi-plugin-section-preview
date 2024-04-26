48bac324d3659fc07d2d470600ae8e2ba87e27a6
Surya Basak <surya.basak@edgewrapper.com>
2024-04-26 13:14:43 +0530

    popup size increased

--- admin/src/utils/axiosInstance.js
+++ admin/src/utils/axiosInstance.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 import { auth } from '@strapi/helper-plugin';
 
 const instance = axios.create({
-  baseURL: process.env.STRAPI_ADMIN_BACKEND_URL,
+  baseURL: process.env.STRAPI_SITE_FRONTEND_URL,
 });
 
 instance.interceptors.request.use(
